eeb3c1bafa41c6d4a6a2237d3a87bc12e045f4a9
Kurt Austin <kurt.austin@gmail.com>
2020-08-06 19:42:48 -0500

    backend working

--- models/todo.js
+++ models/todo.js
@@ -1,4 +1,4 @@
-modules.exports = function(sequelize, DataTypes){
+module.exports = function(sequelize, DataTypes){
     const Todo = sequelize.define("Todo",{
         text:{
             type:DataTypes.STRING,
